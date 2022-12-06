62979008b5c51fbca6eb99e38f32d1c983f47083
fenixbvh683 <manidelabk031@gmail.com>
2022-12-06 10:43:26 -0300

    El servidor ya esta andando

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,20 +1,24 @@
 import { Component, OnInit } from '@angular/core';
-import {Task} from "../../Task"
-import {TASKS} from "../../mock-tasks"
-
+import { TaskService } from '../../service/task.service';
+import { Task } from '../../Task';
 
 @Component({
   selector: 'app-tasks',
   templateUrl: './tasks.component.html',
   styleUrls: ['./tasks.component.css']
 })
-export class TasksComponent {
-  tasks: Task[] = TASKS;
+export class TasksComponent implements OnInit{
+  tasks: Task[] = [];
   
 
-  constructor() { }
+  constructor(
+    private taskService: TaskService
+  ) { }
 
-  ngOnInit(): void{
+  ngOnInit(): void {
+  this.taskService.getTasks().subscribe(tasks => [
+    this.tasks = tasks
+  ]);
 
   }
 
